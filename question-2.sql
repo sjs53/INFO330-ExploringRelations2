9d67851b56b3eea63d28a9bae9e3797c8b9ae224
sjs53 <sjs53@uw.edu>
2023-04-11 22:43:39 -0700

    'second-update'

--- question-2.sql
+++ question-2.sql
@@ -1,2 +1,2 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
-SELECT employees.firstName, employees.lastName, invoices.invoiceId FROM employees JOIN customers JOIN invoices ON employees.employeeId = customers.SupportRepId AND customers.CustomerId = invoices.CustomerId WHERE title = 'Sales Support Agent';
+SELECT employees.firstName, employees.lastName, invoices.* FROM employees JOIN customers JOIN invoices ON employees.employeeId = customers.SupportRepId AND customers.CustomerId = invoices.CustomerId WHERE title = 'Sales Support Agent';
