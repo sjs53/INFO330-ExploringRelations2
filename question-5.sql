63c126d537ab68834c80e3b8519d1096e9831944
sjs53 <sjs53@uw.edu>
2023-04-11 23:37:16 -0700

    'fifth'

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,2 @@
 -- What was the most purchased track of 2013?
+ SELECT tracks.Name FROM invoices JOIN invoice_items JOIN tracks ON tracks.TrackId = invoice_items.TrackId AND invoice_items.InvoiceId = invoices.InvoiceId WHERE InvoiceDate LIKE '2013%' GROUP BY invoices.InvoiceId;
