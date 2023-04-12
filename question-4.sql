46a15e0ba29dfe992ef9f635efea52af5fdeb44c
sjs53 <sjs53@uw.edu>
2023-04-11 23:27:01 -0700

    'fourth'

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,2 @@
 -- Which sales agent made the most in sales in 2010?
+SELECT employees.firstName, employees.lastName FROM invoices JOIN customers JOIN employees ON employees.employeeId = customers.SupportRepId AND customers.CustomerId = invoices.CustomerId WHERE InvoiceDate LIKE '2010%' GROUP BY employees.employeeId;
